cb66817983f0e1e5506ed61ef36ec0382093ceb7
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-28 13:53:46 +0000

    automatically backed up by learn

---
+++ lib/appointment.rb
@@ -0,0 +1,9 @@
+class Appointment 
+  attr_reader :patient, :doctore, :date
+  @@all = []
+  
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+  end
