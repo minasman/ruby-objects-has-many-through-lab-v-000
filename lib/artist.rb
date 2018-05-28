5ef8619bd87368fd21a045270c709b7454f3759c
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-28 12:56:57 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist 
+  @@all = []
+  attr_reader :name 
+  attr_accessor :songs
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def new_song(name, genre)
+    genre = Genre.new(genre)
+    song = Song.new(song, genre)
+    song.artist = self 
+    self.songs << song
+  end
+
+end
