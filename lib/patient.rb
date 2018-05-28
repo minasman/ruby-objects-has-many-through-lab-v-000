2ca5145ea9e85df1f135a64a1643df804c37fa82
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-28 14:25:22 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,11 +1,15 @@
 class Patient 
   attr_reader :name 
+  attr_accessor :appointments
   
   def initialize(name)
     @name = name
+    @appointments = []
   end
   
   def new_appointment(doctor, date)
-    
+    appointment = Appointment.new(self, doctor, date)
+    self.appointments << appointment
+    appointment
   end 
 end
